c166d5630cfed43a7b755c4ae5426360d35fa0fc
RaktimBanerje <braktim99@gmail.com>
2023-07-10 08:39:49 +0530

    Update

--- footer.php
+++ footer.php
@@ -1,4 +1,59 @@
 
+    <footer class="section_padding">
+      <div class="container section_padding">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-3">
+            <ul>
+              <li>
+                <a href="#">My Story</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Public Speaking</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li>
+                <a href="#">My Story</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Public Speaking</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li>
+                <a href="#">My Story</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Public Speaking</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
