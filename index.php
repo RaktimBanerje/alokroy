cd48280eedbfcea486bf69ddb9c092f0ddc515cc
RaktimBanerje <braktim99@gmail.com>
2023-07-10 08:22:52 +0530

    Update

---
+++ index.php
@@ -0,0 +1,157 @@
+
+<?php include('header.php') ?>
+<section>
+    <div class="banner">
+        <div class="overlay">
+            <div class="container h-100 d-flex align-items-center px-md-5 pt-5 pt-md-0">
+                <div class="pt-5 pt-md-0">
+                    <h2 class="title title-1">awareness is</h2>
+                    <h2 class="title title-2">everything</h2>
+                    <p class="text">Make sure all the choices you make in life come from a point <br> of awareness and not ignorance.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container section_padding">
+        <div class="content">
+            <h2 class="title text-center mb-3">dr. alok roy</h2>
+            <p class="text">warikoo is an Internet Entrepreneur based out of India and is one of India’s top content creators. He is also a bestselling author and conducts courses online as an educator. His aim through his content is to make sure all the choices you make in life come from a point of awareness and not ignorance.​</p>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container d-flex justify-content-center">
+        <img src="assets/images/signature.webp" />
+    </div>
+</section>
+
+<section>
+    <div class="content-2 container section_padding">
+        <div class="row">
+            <div class="col-md-3 px-4 text-center">
+                <div class="row justify-content-center">
+                    <i aria-hidden="true" class="fas fa-business-time"></i>
+                </div>
+                <h3 class="my-2">Entrepreneur</h3>
+                <p>warikoo has been an entrepreneur since 2008, was the founding CEO of Groupon India and cofounded nearbuy.com
+                    He is currently working on an education startup.</p>
+            </div>
+            <div class="col-md-3 px-4 text-center">
+                <div class="row justify-content-center">
+                    <i aria-hidden="true" class="fas fa-business-time"></i>
+                </div>
+                <h3 class="my-2">Entrepreneur</h3>
+                <p>warikoo has been an entrepreneur since 2008, was the founding CEO of Groupon India and cofounded nearbuy.com
+                    He is currently working on an education startup.</p>
+            </div>
+            <div class="col-md-3 px-4 text-center">
+                <div class="row justify-content-center">
+                    <i aria-hidden="true" class="fas fa-business-time"></i>
+                </div>
+                <h3 class="my-2">Entrepreneur</h3>
+                <p>warikoo has been an entrepreneur since 2008, was the founding CEO of Groupon India and cofounded nearbuy.com
+                    He is currently working on an education startup.</p>
+            </div>
+            <div class="col-md-3 px-4 text-center">
+                <div class="row justify-content-center">
+                    <i aria-hidden="true" class="fas fa-business-time"></i>
+                </div>
+                <h3 class="my-2">Entrepreneur</h3>
+                <p>warikoo has been an entrepreneur since 2008, was the founding CEO of Groupon India and cofounded nearbuy.com
+                    He is currently working on an education startup.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Social -->
+<section>
+    <div class="container-fluid social section_padding">
+        <h1 class="title text-center">Find alok roy on</h1>
+        <p class="text my-2">ankur warikoo has a strong community of 8Mn+ followers across all popular social media.</p>
+
+        <div class="container pt-5">
+            <div class="row pt-5">
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-youtube"></i>
+                        <h4 class="text">YouTube</h4>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-instagram"></i>
+                        <h4 class="text">Instagram</h4>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-twitter"></i>
+                        <h4 class="text">Twitter</h4>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-spotify"></i>
+                        <h4 class="text">Spotify</h4>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-linkedin-in"></i>
+                        <h4 class="text">Linkedin</h4>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex justify-content-center">
+                    <div class="social-icon d-flex flex-column align-items-center">    
+                        <i aria-hidden="true" class="fab fa-facebook"></i>
+                        <h4 class="text">Facebook</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container gallary section_padding">
+        <h1 class="title text-center">Gallery</h1>
+
+        <div class="row mt-4">
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-1.webp">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-2.webp">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-3.webp">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-4.webp">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-3.webp">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="img-fluid" src="assets/images/gimg-1.webp">
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Blog -->
+<section>
+    <div class="container-fluid social section_padding">
+        <h1 class="title text-center">Read my blog</h1>
+        <p class="text my-2">Read the latest articles from ankur warikoo.</p>
+
+    </div>
+</section>
+
+
+<?php include('footer.php') ?>
